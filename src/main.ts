af4d26de3dcfb55c2e31b0aa1887685c32852338
Florjan96 <florianshiqerukaj@yahoo.com>
2022-07-12 19:30:50 +0200

    fetching and loopin over posts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import './style.css'
 
 let state={
-
+store:[]
 }
+
+
+
+    
+    
+function mainItems(){
+  fetch(`http://localhost:3003/store`)
+  .then(resp=>resp.json())
+  .then(itemsMain=>{
+    state.store=itemsMain
+render()
+  }) 
+}
+mainItems()
+console.log(mainItems())
+window.state=state
+
+
+
+
+
 
 
 
@@ -10,7 +31,13 @@ let state={
 
 function render(){
 
-let header=document.querySelector('.header')
+let appEl=document.querySelector('#app')
+if(!appEl) return
+appEl.textContent=''
+
+
+let header=document.createElement('header')
+header.className='header'
 
 
   let headerLeft=document.createElement('div')
@@ -22,13 +49,11 @@ hollixtonH1.textContent='HOLLIXTON'
 
 let girls=document.createElement('h4')
 girls.textContent='Girls'
-
 let guys=document.createElement('h4')
 guys.textContent='Guys'
 
 let sale=document.createElement('h4')
 sale.textContent='Sale'
-
 let headerRight=document.createElement('div')
 headerRight.className='header-right'
 
@@ -47,5 +72,47 @@ bag.textContent='local_mall'
 headerLeft.append(hollixtonH1,girls,guys,sale)
 headerRight.append(search,person,bag)
 header.append(headerLeft,headerRight)
+appEl.append(header)
+
 }
 render()
+
+
+function proveL(){
+  for(let items of state.store){
+    
+    let singlePostt=document.querySelector('.single-post')
+    if(singlePostt===null)return
+//     <div class="post">
+
+//     <div class="image"> <img src='foto-e-hoxx.png'></div>
+//   <div class="description">Crewneck T-Shirt 3-Pack</div>
+//   <div class="price">$21.99</div>
+// </div>
+
+let post=document.createElement('div')
+post.className='post'
+
+let imageDiv=document.createElement('div')
+imageDiv.className='image'
+
+let img=document.createElement('img')
+img.src='foto-e-hoxx.png'
+
+let description=document.createElement('div')
+description.className='description'
+description.textContent='Crewneck T-Shirt 3-Pack'
+
+let price=document.createElement('div')
+price.className='price'
+price.textContent='$21.99'
+
+imageDiv.append('img')
+post.append(imageDiv,description,price)
+
+singlePostt.append(post)
+  }
+
+}
+
+proveL()
